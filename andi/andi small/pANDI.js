dfaa988de8996b295c2752be09feecbe49548ff6
Ollie Willcox <owillcox@iteratorstesting.com>
2021-06-15 15:58:03 -0400

    Making small code file names match bigger code areas

--- andi/andi small/pANDI.js
+++ andi/andi small/pANDI.js
@@ -1,82 +1,69 @@
 //==========================================//
-//pANDI: lists ANDI (small code)            //
+//pANDI: fake headers ANDI (small code)     //
 //Created By Social Security Administration //
 //==========================================//
 //NOTE: This only contains the code for finding errors and none for displaying the error code
 function init_module() {
-    var pANDI = new AndiModule("4.1.4", "p"); //create pANDI instance
+    //create pANDI instance
+    var pANDI = new AndiModule("4.1.4", "p");
     pANDI.index = 1;
 
-    //This object class is used to store data about each landmark. Object instances will be placed into an array.
-    function List(element, index, closestListItem, listContainer, listContainer_role, closestDesc, isAriaHidden, ariaLabel, ariaLabelledby, ariaRole, ariaLabeledby, alerts) {
+    //This object class is used to store data about each fake header. Object instances will be placed into an array.
+    function FakeHeader(element, index, text, fontSize, fontWeight, nextElement, nextText, nextFontSize, nextFontWeight, isFakeHeader) {
         this.element = element;
         this.index = index;
-        this.closestListItem = closestListItem;
-        this.listContainer = listContainer;
-        this.listContainer_role = listContainer_role;
-        this.closestDesc = closestDesc;
-        // Common Non Focusable Element Attributes
-        this.isAriaHidden = isAriaHidden;
-        this.ariaLabel = ariaLabel;
-        this.ariaLabelledby = ariaLabelledby;
-        this.ariaRole = ariaRole;
-        this.ariaLabeledby = ariaLabeledby;
-        this.alerts = alerts;
+        this.text = text;
+        this.fontSize = fontSize;
+        this.fontWeight = fontWeight;
+        this.nextElement = nextElement;
+        this.nextText = nextText;
+        this.nextFontSize = nextFontSize;
+        this.nextFontWeight = nextFontWeight;
+        this.isFakeHeader = isFakeHeader;
     }
 
-    //This object class is used to keep track of the lists on the page
-    function Lists() {
-        this.list = [];
+    //This object class is used to keep track of the fake headers on the page
+    function FakeHeaders() {
+        this.FakeHeaderList = [];
         this.count = 0;
-        this.liCount = 0;
-        this.ddCount = 0;
-        this.dtCount = 0;
-        this.listItemRoleCount = 0;
     }
 
     //This analyzes the test page for graphics/image related markup relating to accessibility
     pANDI.analyze = function () {
-        pANDI.lists = new Lists();
-        $(TestPageData.allElements).each(function () { //Loop through every visible element
-            if ($(this).isSemantically("[role=listitem],[role=list]", "ol,ul,li,dl,dd,dt")) {
-                var closestListItem = "";
-                var listContainer = "";
-                var listContainer_role = "";
-                var closestDesc = "";
-                var ariaLabel = $(this).attr("aria-label");
-                var ariaLabelledby = $(this).attr("aria-labelledby");
-                var ariaRole = $(this).attr("aria-role");
-                var ariaLabeledby = $(this).attr("aria-labeledby");
-                if ($(this).is("[role=listitem]")) {
-                    pANDI.lists.listItemRoleCount += 1;
-                    closestListItem = $(this).closest("[role=list]").length;
-                    if (!closestListItem) { //Is the listitem contained by an appropriate list container?
-                        alert = [alert_0079, ["[role=listitem]", "[role=list]"]];
-                    }
-                } else if ($(this).is("li")) {
-                    pANDI.lists.liCount += 1;
-                    var listContainer = $(this).closest("ol,ul");
-                    if (!$(listContainer).length) {
-                        alert = [alert_0079, ["&lt;li&gt;", "&lt;ol&gt; or &lt;ul&gt;"]];
-                    } else { //check if listContainer is still semantically a list
-                        var listContainer_role = $(listContainer).attr("role");
-                        if (listContainer_role && listContainer_role !== "list")
-                            alert = [alert_0185, [listContainer_role]];
-                    }
-                } else if ($(this).is("dd,dt")) {
-                    pANDI.lists.ddCount += 1;
-                    closestDesc = !$(this).closest("dl").length;
-                    if (closestDesc) {
-                        alert = [alert_007A];
+        pANDI.fakeHeaders = new FakeHeaders();
+        //Loop through every visible element
+        $(TestPageData.allElements).each(function () {
+            if ($(this).is("p,div,span,strong,em")) {
+                var text = $.trim($(this).text());
+                var fontSize = parseInt($(this).css("font-size"));
+                var fontWeight = $(this).css("font-weight");
+                var nextElement = $(this).next().filter(":visible");
+                var nextText = $.trim($(nextElement).text());
+                var nextFontSize = parseInt($(nextElement).css("font-size"));
+                var nextFontWeight = $(nextElement).css("font-weight");
+                var isFakeHeading = false;
+                if (text.length > 0 && text.length < 30) {
+                    var fakeHeadingIsBold = (fontWeight === "bold" || fontWeight === "bolder" || fontWeight > 700);
+                    if (fontSize > 22 || (fakeHeadingIsBold && fontSize > 15)) { //fontSize is greater than size limit
+                        if (nextText !== "") { //next element has text
+                            var nextIsBold = (nextFontWeight === "bold" || nextFontWeight === "bolder" || nextFontWeight > 700);
+                            if (nextFontSize < fontSize) {
+                                //next element's font-size is smaller than fakeHeading font-size
+                                isFakeHeading = true;
+                            } else if (fakeHeadingIsBold && !nextIsBold) {
+                                //next element's font-weight is lighter than fakeHeading font-weight
+                                isFakeHeading = true;
+                            }
+                        }
                     }
                 }
-                andiData = new AndiData(this);
-                andiCheck.commonNonFocusableElementChecks(andiData, $(this));
-                AndiData.attachDataToElement(this);
+                if (isFakeHeading) { //Since pANDI has not found a heading yet, check if this element is a fake headings
+                    andiData = new AndiData(this);
 
-                //Add to the lists array
-                pANDI.lists.list.push(new List(this, pANDI.index, closestListItem, listConatainer, listContainer_role, closestDesc, andiData.isAriaHidden, ariaLabel, ariaLabelledby, ariaRole, ariaLabeledby, ""));
-                pANDI.lists.count += 1;
+                    alert = [alert_0190];
+                    AndiData.attachDataToElement(this);
+                }
+                pANDI.fakeHeaders.FakeHeaderList.push(new FakeHeader(this, pANDI.index, text, fontSize, fontWeight, nextElement, nextText, nextFontSize, nextFontWeight, pANDI.isFakeHeading(this)))
                 pANDI.index += 1;
             }
         });
