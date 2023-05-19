2a7bbcf20009e351f39fa6d7456f1efc330935c3
Ollie Willcox <owillcox@iteratorstesting.com>
2023-05-19 11:05:28 -0400

    Fixing results

--- andi/nandi.js
+++ andi/nandi.js
@@ -44,7 +44,7 @@ nANDI.viewList_tableReady = false;
 nANDI.analyze = function(objectClass){
 
     //Variables used to build the links/buttons list array.
-    var href, nameDescription, alerts, target, linkPurpose, accesskey, alertIcon, alertObject, relatedElement, nonUniqueIndex, ambiguousIndex;
+    var nameDescription, alerts, accesskey, alertIcon, alertObject, relatedElement, nonUniqueIndex;
 
     //Loop through every visible element and run tests
     $(TestPageData.allElements).each(function(){
