8cd43cfa9dc3ee71b42e7a4894b31cc01486d481
Ollie Willcox <owillcox@iteratorstesting.com>
2023-05-24 15:51:33 -0400

    Remove unneeded code from iANDI

--- andi/iandi.js
+++ andi/iandi.js
@@ -28,58 +28,10 @@ var showStartUpSummaryText = "To test the contents of <span class='ANDI508-modul
 //This function adds the finishing touches and functionality to ANDI's display once it's done scanning the page.
 //Inserts some counter totals, displays the accesskey list
 iANDI.results = function(){
-    var iframesSelectionMenu = "";
-    var iframesSelectionLinks = "";
-
-    $("#ANDI508-testPage .ANDI508-element").each(function(){
-        //Build iFrame List
-        iframesSelectionLinks += "<li><a href='javascript:void(0)' data-andi508-relatedindex='"+$(this).attr('data-andi508-index')+"'>";
-        if($(this).attr("src"))
-            iframesSelectionLinks += $(this).attr("src");
-        else
-            iframesSelectionLinks += "No src";
-        iframesSelectionLinks += "</a></li>";
-    });
-    //iframes contain body content
-    if(iframesSelectionLinks){
-        iframesSelectionMenu += "<p>Select iframe to open in a new tab, then launch ANDI.</p>"+
-        "<ol>" + iframesSelectionLinks + "</ol>";
-    }
-
-    $("#ANDI508-additionalPageResults").append("<button id='ANDI508-viewIframeList-button' class='ANDI508-viewOtherResults-button' aria-expanded='false'>"+listIcon+"view iframe list</button><div id='iANDI508-iframeList-container' class='ANDI508-viewOtherResults-expanded' tabindex='0'><div class='ANDI508-scrollable'>"+iframesSelectionMenu+"</div></div>");
-
     $("#iANDI508-iframeList-container").find("a").click(function(){
         var relatedIndex = $(this).attr("data-andi508-relatedindex");
         var relatedIframe = $("#ANDI508-testPage .ANDI508-element[data-andi508-index="+relatedIndex+"]");
         iANDI.openIframeInNewWindow(relatedIframe);
-    });
-
-    //Define outline button
-    $("#ANDI508-viewIframeList-button").click(function(){
-        if($(this).attr("aria-expanded") === "true"){
-            //hide iframe list, show alert list
-            $("#iANDI508-iframeList-container").slideUp(AndiSettings.andiAnimationSpeed);
-            $("#ANDI508-alerts-list").show();
-            $(this)
-                .addClass("ANDI508-viewOtherResults-button-expanded")
-                .html(listIcon+"view iframe list")
-                .attr("aria-expanded","false")
-                .removeClass("ANDI508-viewOtherResults-button-expanded ANDI508-module-action-active");
-        }
-        else{
-            //show iframe list, hide alert list
-            $("#ANDI508-alerts-list").hide();
-
-            andiSettings.minimode(false);
-            $(this)
-                .html(listIcon+"hide iframe list")
-                .attr("aria-expanded","true")
-                .addClass("ANDI508-viewOtherResults-button-expanded ANDI508-module-action-active")
-                .find("img").attr("src",icons_url+"list-on.png");
-            $("#iANDI508-iframeList-container").slideDown(AndiSettings.andiAnimationSpeed).focus();
-        }
-        andiResetter.resizeHeights();
-        return false;
     });
 
     //For iframe list links, add hoverability, focusability, clickability
