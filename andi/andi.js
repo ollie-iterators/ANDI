110b9ca7d81594b5c1e55d9e1121423eb9175727
Ollie Willcox <owillcox@iteratorstesting.com>
2023-06-12 14:08:58 -0400

    Add back .cssText

--- andi/andi.js
+++ andi/andi.js
@@ -4162,7 +4162,7 @@ var jqueryDownloadSource = "https://ajax.googleapis.com/ajax/libs/jquery/"; //wh
 
         var computedStyle = objectClass.list[index].elementList[0].style;
         //var computedStyle = window.getComputedStyle(objectClass.list[index].elementList[0], "none");
-        $(objectClass.list[index].elementList[0]).attr("data-andi508-computedStyle", computedStyle.toString());
+        $(objectClass.list[index].elementList[0]).attr("data-andi508-computedStyle", computedStyle.cssText.toString());
         if (!attributesToAdd.includes("data-andi508-computedStyle")) {
             attributesToAdd.push("data-andi508-computedStyle");
         }
