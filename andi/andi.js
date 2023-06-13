0f2e14a36c5054d0887354f6096643b38994ff2f
Ollie Willcox <owillcox@iteratorstesting.com>
2023-06-13 12:13:31 -0400

    Fixing add attributes from data

--- andi/andi.js
+++ andi/andi.js
@@ -4166,7 +4166,7 @@ var jqueryDownloadSource = "https://ajax.googleapis.com/ajax/libs/jquery/"; //wh
             var dataKeys = Object.keys(data);
             for (var d = 0; d < dataKeys.length; d += 1) {
                 if (!attributesToAdd.includes(dataKeys[d])) {
-                    attributesToAdd.push("data-andi508-" + data[dataKeys[d]]);
+                    attributesToAdd.push("data-andi508-" + dataKeys[d]);
                     $(objectClass.list[index].elementList[0]).attr("data-andi508-" + dataKeys[d], data[dataKeys[d]]);
                 }
             }
