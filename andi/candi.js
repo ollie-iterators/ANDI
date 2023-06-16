61d1beb5aedd22bcf3d3b559a0ed0de5df2696c6
Ollie Willcox <owillcox@iteratorstesting.com>
2023-06-16 13:51:07 -0400

    Changing pacity to opacity

--- andi/candi.js
+++ andi/candi.js
@@ -430,7 +430,7 @@ cANDI.getContrast = function(fgElement){
         max:                max,
         ratio: 				ratio,
         semiTransparency:	semiTransparency,
-        pacity:			    $(fgElement).css("opacity"),
+        opacity:			$(fgElement).css("opacity"),
         bgImage:			$(bgElement).css("background-image"),
         size:				parseFloat($(fgElement).css("font-size")),
         weight:				$(fgElement).css("font-weight"),
