d9b8e20c336f33b88f5e259474c13da9a828063a
Ollie Willcox <owillcox@iteratorstesting.com>
2023-06-16 12:08:44 -0400

    changing opacity to opacityVal

--- andi/candi.js
+++ andi/candi.js
@@ -430,7 +430,7 @@ cANDI.getContrast = function(fgElement){
         max:                max,
         ratio: 				ratio,
         semiTransparency:	semiTransparency,
-        opacity:			opacity,
+        opacityVal:			opacity,
         bgImage:			$(bgElement).css("background-image"),
         size:				parseFloat($(fgElement).css("font-size")),
         weight:				$(fgElement).css("font-weight"),
