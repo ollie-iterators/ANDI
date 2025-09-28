bb7cecd4523dc659578e8333388a77be0f73d545
Ollie Willcox <owillcox@iteratorstesting.com>
2025-09-28 17:01:58 -0400

    Change name of links

--- andi/mandi.js
+++ andi/mandi.js
@@ -122,7 +122,7 @@ function PossibleLinks(){
 
 // This object class is used to keep track of the table information
 function TableInfo() {
-    this.tableMode      = "Links";
+    this.tableMode      = "Possible Links";
     this.cssProperties  = [];
     this.buttonTextList = [];
     this.tabsTextList   = [];
