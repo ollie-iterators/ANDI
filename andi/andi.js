b37216aea06474c72efcaa5c5741694afd0a7b92
Ollie Willcox <owillcox@iteratorstesting.com>
2025-09-29 04:54:18 -0400

    Changing title

--- andi/andi.js
+++ andi/andi.js
@@ -556,11 +556,11 @@ function andiReady(){
             //vANDI
             "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-v' aria-label='data tables'>data tables</button>"+
             //wANDI
-            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-w' aria-label='data tables'>data tables</button>"+
+            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-w' aria-label='data table rows'>data table rows</button>"+
             //xANDI
-            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-x' aria-label='data tables'>data tables</button>"+
+            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-x' aria-label='data table headers'>data table headers</button>"+
             //yANDI
-            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-y' aria-label='data tables'>data tables</button>"+
+            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-y' aria-label='data table cells'>data table cells</button>"+
             //oANDI
             "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-o' aria-label='certain headers'>certain headers</button>"+
             //pANDI
