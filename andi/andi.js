d51c11126daa5faf99ace611b06b9bc0d9cfa2f4
Ollie Willcox <owillcox@iteratorstesting.com>
2025-09-29 08:14:38 -0400

    Testing changing to wANDI

--- andi/andi.js
+++ andi/andi.js
@@ -554,7 +554,7 @@ function andiReady(){
             //uANDI
             "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-u' aria-label='strange tables'>strange tables</button>"+
             //vANDI
-            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-v' aria-label='data tables'>data tables</button>"+
+            "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-w' aria-label='data tables'>data tables</button>"+
             //wANDI
             "<button role='menuitem' class='ANDI508-moduleMenu-option' id='ANDI508-moduleMenu-button-w' aria-label='data table rows'>data table rows</button>"+
             //xANDI
