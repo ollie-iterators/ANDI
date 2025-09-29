e872728cc8c5e8e2d3f1dc86d3b3e12031fc033a
Ollie <46904826+ollie-iterators@users.noreply.github.com>
2025-09-29 11:31:04 -0400

    Fixing new DataTable code

--- andi/zandi.js
+++ andi/zandi.js
@@ -99,7 +99,7 @@ zANDI.analyze = function(objectClass){
 
 			if($(this).isSemantically(["table","grid","treegrid"],"table")){
 				//It's a data table
-                objectClass.list.push(new DataTable([cell[0]], objectClass.list.length + 1, "", "", ""));
+                objectClass.list.push(new DataTable([this], objectClass.list.length + 1, "", "", ""));
                 andiBar.getAttributes(objectClass, objectClass.list.length - 1);
                 objectClass.elementNums[0] += 1;
                 objectClass.elementStrings[0] = "Data Table";
