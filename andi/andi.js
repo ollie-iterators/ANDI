b56bcc6e38e85a0ec53e834e0e1aef052fb8dfd8
Ollie Willcox <owillcox@iteratorstesting.com>
2025-10-16 15:22:48 -0400

    Revert "Test code"
    This reverts commit ad5e294c727fa5d59286667d12f3f2afe9f07388.

--- andi/andi.js
+++ andi/andi.js
@@ -331,7 +331,6 @@ AndiModule.launchModule = function(module){
             AndiModule.module = module; //Set current module to launched module
             AndiModule.activeActionButtons = {}; //Reset action buttons
         }
-        var andiResults = new AndiResults();
 
         testPageData = new TestPageData(); //get fresh test page data
 
