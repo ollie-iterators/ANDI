0f34a67e9de3e36670873a6b15c1563f9a07126d
Ollie Willcox <owillcox@iteratorstesting.com>
2025-10-16 17:05:50 -0400

    Removing code

--- andi/zandi.js
+++ andi/zandi.js
@@ -1529,7 +1529,6 @@ function DataTables() {
 function TableInfo() {
     this.tableMode      = "Data Tables";
     this.buttonTextList = ["Table Markup"];
-    this.tabsTextList   = [];
 }
 
 zANDI.dataTables = new DataTables();
